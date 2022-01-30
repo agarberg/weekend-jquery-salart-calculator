19563dfc546e8416ef4a4d19cd709943e2c6ae4c
Adam Garberg <adamgarberg@gmail.com>
2022-01-29 20:43:07 -0600

    functions functional

--- client.js
+++ client.js
@@ -1,44 +1,82 @@
 let employeeArray = [];
 
 function newEmployee(fNameInput, lNameInput, empIdInput, jobTitleInput, annSalaryInput){ 
-    console.log('in new employee');
+    console.log('in new employee', fNameInput, lNameInput, empIdInput, jobTitleInput, annSalaryInput);
 const employeeObj = {
-    firstName: fNameInput,
-    lastName: lNameInput,
-    employeeId: empIdInput,
-    employeeTitle: jobTitleInput,
-    employeeSalary: annSalaryInput,
+    first: fNameInput,
+    last: lNameInput,
+    id: empIdInput,
+    title: jobTitleInput,
+    salary: annSalaryInput,
 }
 employeeArray.push(employeeObj);
-return true;
+totalMonthlyCostAdder()
+console.log(employeeArray);
+
 }
 
 $(document).ready(submit)
 function submit() {
 $('#submitButton').on('click',(submitButton));
+
 }
     //gather input values 
     function submitButton() {
         let firstName = $('#fNameInput').val(); 
         let lastName = $('#lNameInput').val();
-        let employeeId= $('#empIdInput').val()
-        let employeeTitle= $('#jobTitleInput').val()
-        let employeeSalary= $('#annSalaryInput').val()
-        newEmployee (firstName, lastName, employeeId, employeeTitle, employeeSalary)
+        let employeeId= $('#empIdInput').val();
+        let employeeTitle= $('#jobTitleInput').val();
+        let employeeSalary= $('#annSalaryInput').val();
+        newEmployee (firstName, lastName, employeeId, employeeTitle, employeeSalary);
+       
+
     $('#firstNameRow').append(`<tr><td>${firstName}</td></tr>`);
     $('#lastNameRow').append(`<tr><td>${lastName}</td></tr>`);
     $('#empIdRow').append(`<tr><td>${employeeId}</td></tr>`);
     $('#empTitleRow').append(`<tr><td>${employeeTitle}</td></tr>`);
     $('#annSalaryRow').append(`<tr><td>${employeeSalary}</td></tr>`);
+    $('#delete').append('<tr><td><button id="deleteButton" type="button">Delete</button></td></tr>');
+    // $('#deleteButton').on('click', (deleteClick));    
     }
 
-    // console.log(firstName);
-    // console.log(lastName);
-    // console.log(employeeId);
-    // console.log(employeeTitle);
-    // console.log(employeeSalary);
-    
+
+    function totalMonthlyCostAdder(){
+        console.log('in total monthly cost adder');
+        let totalMonthlyCost = 0;
+        for (let i = 0; i < employeeArray.length; i++){
+            totalMonthlyCost += Number(employeeArray[i].salary); 
+        }     
+        console.log(totalMonthlyCost);
+        $('#total').text(`${totalMonthlyCost}`);
+    }
+        
+
     
 
+    
+ 
 
-// console.log(employeeArray)
+    // function deleteClick(){
+    //     console.log('in delete click');
+        
+    //     console.log('what is', this);
+        
+    //     $(this).remove()
+
+
+        // $('#firstNameRow').val();
+        // $('#lastNameRow').val();
+        // $('#empIdRow').val();
+        // $('#empTitleRow').val();
+        // $('#annSalaryRow').val();
+        // $('#delete').val(); 
+    // }
+
+// let sum = 0;
+//   function sumScores(array){
+//     for (let i = 0; i < array.length; i++){
+//       sum += array[i].score;
+//     }
+//     return sum;
+//   }
+
